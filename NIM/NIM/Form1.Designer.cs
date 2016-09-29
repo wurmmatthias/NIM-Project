ecba4ad408f3e167004218f7f3cbe71b4b74d8a8
M.Wurm0706 <M.Wurm0706@PC-D307-06>
2016-09-29 13:36:04 +0200

    Initial

---
+++ NIM/NIM/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace NIM
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.one = new System.Windows.Forms.Button();
+            this.two = new System.Windows.Forms.Button();
+            this.three = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // one
+            // 
+            this.one.Location = new System.Drawing.Point(26, 19);
+            this.one.Name = "one";
+            this.one.Size = new System.Drawing.Size(168, 71);
+            this.one.TabIndex = 0;
+            this.one.Text = "1";
+            this.one.UseVisualStyleBackColor = true;
+            this.one.Click += new System.EventHandler(this.one_Click);
+            // 
+            // two
+            // 
+            this.two.Location = new System.Drawing.Point(236, 19);
+            this.two.Name = "two";
+            this.two.Size = new System.Drawing.Size(168, 71);
+            this.two.TabIndex = 1;
+            this.two.Text = "2";
+            this.two.UseVisualStyleBackColor = true;
+            this.two.Click += new System.EventHandler(this.two_Click);
+            // 
+            // three
+            // 
+            this.three.Location = new System.Drawing.Point(446, 19);
+            this.three.Name = "three";
+            this.three.Size = new System.Drawing.Size(168, 71);
+            this.three.TabIndex = 2;
+            this.three.Text = "3";
+            this.three.UseVisualStyleBackColor = true;
+            this.three.Click += new System.EventHandler(this.three_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.one);
+            this.groupBox1.Controls.Add(this.three);
+            this.groupBox1.Controls.Add(this.two);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(640, 100);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Actions";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(686, 255);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "NIM - Felix Dango, Adrian Probst, Matthias Wurm";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button one;
+        private System.Windows.Forms.Button two;
+        private System.Windows.Forms.Button three;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ NIM/NIM/Form1.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NIM
+{
+    public partial class Form1 : Form
+    {
+
+        int space = 10;
+        int counter = 0;
+        int loopi = 0;
+        int room = 10;
+        int endcounter = 0;
+        int k = 50;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+
+        }
+
+        private void DrawItBlack(int x)
+        {
+            System.Drawing.SolidBrush myBrush = new System.Drawing.SolidBrush(System.Drawing.Color.Black);
+            System.Drawing.Graphics formGraphics;
+            formGraphics = this.CreateGraphics();
+            formGraphics.FillEllipse(myBrush, new Rectangle(x, 150, 50, 50));
+            myBrush.Dispose();
+            formGraphics.Dispose();
+
+        }
+
+        private void DrawItRed(int x)
+        {
+            System.Drawing.SolidBrush myBrush = new System.Drawing.SolidBrush(System.Drawing.Color.Red);
+            System.Drawing.Graphics formGraphics;
+            formGraphics = this.CreateGraphics();
+            formGraphics.FillEllipse(myBrush, new Rectangle(x, 150, 50, 50));
+            myBrush.Dispose();
+            formGraphics.Dispose();
+
+        }
+
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            loopi = 11;
+            space = 10;
+            while (loopi > 0)
+            {
+                space = space + k;
+                DrawItBlack(space);
+                loopi = loopi - 1;
+            }
+
+            
+        }
+
+        private void one_Click(object sender, EventArgs e)
+        {
+            counter++;
+            endcounter++;
+
+            loopi = counter;
+            while (loopi > 0)
+            {
+                room = room + k;
+                DrawItRed(room);
+                loopi = loopi - 1;
+            }
+            counter = 0;
+            if (endcounter >= 11)
+            {
+
+                MessageBox.Show("Du hast verloren! ", "Spielende",MessageBoxButtons.OK, MessageBoxIcon.Information);
+                space = 10;
+                room = 10;
+                loopi = 11;
+                while (loopi > 0)
+                {
+                    space = space + k;
+                    DrawItBlack(space);
+                    loopi = loopi - 1;
+                }
+                endcounter = 0;
+
+            }
+        }
+
+        private void two_Click(object sender, EventArgs e)
+        {
+            if (endcounter <= 8)
+            {
+                counter = 2;
+                endcounter = endcounter + 2;
+                loopi = counter;
+                while (loopi > 0)
+                {
+                    room = room + k;
+                    DrawItRed(room);
+                    loopi = loopi - 1;
+                }
+                counter = 0;
+                if (endcounter >= 11)
+                {
+
+                    MessageBox.Show("Du hast verloren! ", "Spielende", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    space = 10;
+                    room = 10;
+                    loopi = 11;
+                    while (loopi >= 0)
+                    {
+                        space = space + k;
+                        DrawItBlack(space);
+                        loopi = loopi - 1;
+                    }
+                    endcounter = 0;
+
+                }
+            }
+            else
+            {
+
+                MessageBox.Show("Du hast verloren! ", "Spielende", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                space = 10;
+                room = 10;
+                loopi = 10;
+                while (loopi >= 0)
+                {
+                    space = space + k;
+                    DrawItBlack(space);
+                    loopi = loopi - 1;
+                }
+                endcounter = 0;
+            }
+        }
+    
+
+        private void three_Click(object sender, EventArgs e)
+        {
+            if (endcounter <= 7) {
+                counter = 3;
+                endcounter = endcounter + 3;
+                loopi = counter;
+                while (loopi > 0)
+                {
+                    room = room + k;
+                    DrawItRed(room);
+                    loopi = loopi - 1;
+                }
+                counter = 0;
+                if (endcounter >= 11)
+                {
+
+                    MessageBox.Show("Du hast verloren! ", "Spielende", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    space = 10;
+                    room = 10;
+                    loopi = 11;
+                    while (loopi >= 0)
+                    {
+                        space = space + k;
+                        DrawItBlack(space);
+                        loopi = loopi - 1;
+                    }
+                    endcounter = 0;
+
+                }
+            } else {
+
+                MessageBox.Show("Du hast verloren! ", "Spielende", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                space = 10;
+                room = 10;
+                loopi = 10;
+                while (loopi >= 0)
+                {
+                    space = space + k;
+                    DrawItBlack(space);
+                    loopi = loopi - 1;
+                }
+                endcounter = 0;
+            }
+        }
+    }
+}
